2962eaee949cdac83c71a773722782d537c2a63d
Levi <lsbvisser@gmail.com>
2022-10-05 12:51:16 +0200

    Add sid to global scope

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from flask import Flask, render_template, request
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+
 app = Flask(__name__)
+
+sid = SentimentIntensityAnalyzer()
 
 messages = [
     {'title': "Message one", 'content': "Message one content"},
@@ -39,7 +42,6 @@ def create():
 def analyze_tweet(tweet_text):
     ss_list = []
     for sentence in nltk.sent_tokenize(tweet_text):
-        sid = SentimentIntensityAnalyzer()
         ss_list.append(sid.polarity_scores(sentence))
 
     ss = {'neg': 0.0, 'neu': 0.0, 'pos': 0.0, 'compound': 0.0}
