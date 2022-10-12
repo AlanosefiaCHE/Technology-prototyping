674dd9118d537a02c9ecdec5e3f67a5c2826ad10
Alan Osefia <amosefia@student.che.nl>
2022-10-12 12:38:20 +0200

    Displacy added to data extraction.

--- DataExtrac.py
+++ DataExtrac.py
@@ -1,9 +1,13 @@
 import spacy
 import scispacy
 from scispacy.linking import EntityLinker
+from spacy import displacy
 nlp = spacy.load("en_core_sci_lg")
 nlp.add_pipe("scispacy_linker", config={"linker_name": "umls", "max_entities_per_mention": 3})
 doc = nlp("Patient has prostate cancer with metastatic disease to his bladder. He got it because of the new paracetamol that Pfizer made in 2008.")
+displacy.serve(doc, style="ent")
+        
+
 linker = nlp.get_pipe("scispacy_linker")
 fmt_str = "{:<20}| {:<10}| {:<32}| {:<20}"
 print(fmt_str.format("Entity", "1st CUI", "Canonical Name", "Definition"))
