cddde556e82536db92311101f584f564e6582c55
Levi <lsbvisser@gmail.com>
2022-10-18 17:35:31 +0200

    Cleaned up frontend

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/", methods=('GET', 'POST'))
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if request.method == 'POST':
+        print(request.form)
