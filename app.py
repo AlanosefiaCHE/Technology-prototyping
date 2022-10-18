717c098effa9decac3378384453bf4d3a76e47c1
Levi <lsbvisser@gmail.com>
2022-10-18 12:02:28 +0200

    Prototype backend

--- app.py
+++ app.py
@@ -7,54 +7,83 @@ from flask import Flask, render_template, request
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+import matplotlib.pyplot as pyplot
+
+import base64
+from io import BytesIO
 
 app = Flask(__name__)
 
 sid = SentimentIntensityAnalyzer()
 
-messages = [
-    {'title': "Message one", 'content': "Message one content"},
-    {'title': "Message two", 'content': "Message two content"}
-]
 
-
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    tweets = get_tweets()
-    for tweet in tweets:
-        analyze_tweet(tweet)
+    if request.method == 'GET':
+        return render_template('form.html')
 
-    return render_template('display.html', messages=messages)
-
-
-@app.route('/create', methods=('GET', 'POST'))
-def create():
     if request.method == 'POST':
-        if not request.form['title'] or not request.form['content']:
-            return
+        print(request.form)
 
-        messages.append(
-            {'title': request.form['title'], 'content': request.form['content']})
+        tweets = get_tweets()
+        tweets_analysis = analyze_tweets(tweets)
 
-    return render_template('form.html')
+        pyplot.ylim((-1.0, 1.0))
+        pyplot.plot(tweets_analysis[0], label="neg")
+        pyplot.plot(tweets_analysis[1], label="neu")
+        pyplot.plot(tweets_analysis[2], label="pos")
+        pyplot.plot(tweets_analysis[3], label="comp")
+
+        pyplot.legend()
+        pyplot.plot()
+
+        tmpfile = BytesIO()
+        pyplot.savefig(tmpfile, format='svg')
+        encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+
+        return render_template('display.html', graph=encoded)
+
+    else:
+        return
 
 
-def analyze_tweet(tweet_text):
-    ss_list = []
-    for sentence in nltk.sent_tokenize(tweet_text):
-        ss_list.append(sid.polarity_scores(sentence))
+def analyze_tweets(tweets_text):
+    # Reken het sentiment uit van alle tweets in de tweet_list
+    # Maak lijsten met de scores op verschillende vlakken voor de tweets
+    neg_scores_tweets = []
+    neu_scores_tweets = []
+    pos_scores_tweets = []
+    cmp_scores_tweets = []
 
-    ss = {'neg': 0.0, 'neu': 0.0, 'pos': 0.0, 'compound': 0.0}
-    for e in ss_list:
-        ss['neg'] += e['neg']
-        ss['neu'] += e['neu']
-        ss['pos'] += e['pos']
-        ss['compound'] += e['compound']
+    # Loop door de lijst met tweets heen.
+    for tweet in tweets_text:
+        # Maak lijsten met de verschillende scores op verschillende vlakken voor de zinnen in de tweets
+        neg_sentences = []
+        neu_sentences = []
+        pos_sentences = []
+        cmp_sentences = []
 
-    for k in ss:
-        ss[k] /= len(ss)
+        # Tokenize de tweet (splits de tweet op in zinnen)
+        sentences = nltk.tokenize.sent_tokenize(tweet)
 
-    print(ss)
+        # Loop door de lijst met alle zinnen van de tweet
+        for sentence in sentences:
+            # Bereken het sentiment van de zin
+            ss = SentimentIntensityAnalyzer().polarity_scores(sentence)
+
+            # Voeg de score van de zin toe aan de lijst met de scores voor de zinnen in de tweet
+            neg_sentences.append(ss["neg"])
+            neu_sentences.append(ss["neu"])
+            pos_sentences.append(ss["pos"])
+            cmp_sentences.append(ss["compound"])
+
+        # Bereken het gemiddelde van de verschillende scores van de zinnen in een tweet en voeg deze toe aan de lijst van scores voor de tweets
+        neg_scores_tweets.append(sum(neg_sentences) / len(neg_sentences))
+        neu_scores_tweets.append(sum(neu_sentences) / len(neu_sentences))
+        pos_scores_tweets.append(sum(pos_sentences) / len(pos_sentences))
+        cmp_scores_tweets.append(sum(cmp_sentences) / len(cmp_sentences))
+
+        return neg_sentences, neu_sentences, pos_sentences, cmp_sentences
 
 
 def get_tweets():
@@ -63,3 +92,6 @@ def get_tweets():
 
     return deep_translator.GoogleTranslator(
         source="auto", target="en").translate_batch(data)
+
+# Weergeven: Grafiek, 3 selecties voor positief negatief en neutraal. Die dan daaronder tweets laten zien.
+# Tweets weergeven met een bepaalde score range.
