714cbc9de6b3953aeac383aa0ed9995d40097eb7
Alan Osefia <amosefia@student.che.nl>
2022-10-19 11:18:46 +0200

    fix

--- DataExtrac.py
+++ DataExtrac.py
@@ -5,7 +5,6 @@ from spacy import displacy
 nlp = spacy.load("en_core_sci_lg")
 nlp.add_pipe("scispacy_linker", config={"linker_name": "umls", "max_entities_per_mention": 3})
 doc = nlp("Patient has prostate cancer with metastatic disease to his bladder. He got it because of the new paracetamol that Pfizer made in 2008.")
-displacy.serve(doc, style="ent")
         
 
 linker = nlp.get_pipe("scispacy_linker")
@@ -15,3 +14,4 @@ for entity in doc.ents:
     first_cuid = entity._.kb_ents[0][0]
     kb_entry = linker.kb.cui_to_entity[first_cuid]
     print(fmt_str.format(entity.text, first_cuid, kb_entry.canonical_name, kb_entry.definition[0:15] + "..."))
+displacy.serve(doc, style="ent")
