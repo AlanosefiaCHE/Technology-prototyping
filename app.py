302b37076d10254b513bbfc1941972687186b0a3
Levi <lsbvisser@gmail.com>
2022-10-19 17:12:06 +0200

    Add error handling to form

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ CLIENT = tweepy.Client(
 
 SID = SentimentIntensityAnalyzer()
 
+ERR_INVALID_USERNAME = 'Username not found'
+ERR_INVALID_OPTION = 'Invalid option selected'
+ERR_NO_USERNAME = 'Empty username'
+ERR_NO_FROMDATE = 'Empty from date'
+ERR_NO_UNTILDATE = 'Empty until date'
+ERR_NO_AMOUNT = 'Empty amount'
+
 
 @app.route("/", methods=('GET', 'POST'))
 def index():
@@ -27,16 +34,38 @@ def index():
     if request.method == 'POST':
         form = request.form
 
-        id = CLIENT.get_users(usernames=[form['name']])['data'][0]['id']
-        from_date = datetime.strptime(form['from'], FORM_DATE_STRING)
-
-        if form['rangeOrAmountFrom'] == 'range':
-            until_date = datetime.strptime(form['until'], FORM_DATE_STRING)
-            tweets = get_tweets(id=id, from_date=from_date, until_date=until_date)
-        elif form['rangeOrAmountFrom'] == 'amountFrom':
-            tweets = get_tweets(id=id, max_results=int(form['amount']), from_date=from_date)
+        name = form['name']
+        if name:
+            id = CLIENT.get_users(usernames=[name])
+            if 'data' in id:
+                id = id['data'][0]['id']
+            else:
+                return ERR_INVALID_USERNAME, 400
         else:
-            return 'Invalid option selected', 400
+            return ERR_NO_USERNAME, 400
+
+        fromd = form['from']
+        if fromd:
+            from_date = datetime.strptime(fromd, FORM_DATE_STRING)
+        else:
+            return ERR_NO_FROMDATE, 400
+
+        range_or_amount_from = form['rangeOrAmountFrom']
+        if range_or_amount_from == 'range':
+            until = form['until']
+            if until:
+                until_date = datetime.strptime(until, FORM_DATE_STRING)
+                tweets = get_tweets(id=id, from_date=from_date, until_date=until_date)
+            else:
+                return ERR_NO_UNTILDATE, 400
+        elif range_or_amount_from == 'amountFrom':
+            amount = form['amount']
+            if amount:
+                tweets = get_tweets(id=id, max_results=int(amount), from_date=from_date)
+            else:
+                return ERR_NO_AMOUNT, 400
+        else:
+            return ERR_INVALID_OPTION, 400
 
         translated_tweets = translate_tweets(tweets)
         scores_tweets = analyze_tweets(translated_tweets)
@@ -86,11 +115,11 @@ def analyze_tweets(tweets):
 
 def plot_tweets(tweet_scores):
     """Plot_tweets takes in a list of numbers and returns a pygal plot encoded in data_uri."""
-    # TODO: Disable js.
-    line_chart = pygal.Line(show_legend=False, js=[''])
-    line_chart.add('Compound score', tweet_scores)
-
+    line_chart = pygal.Line(
+        show_legend=False,
+        x_title='Tweet number (new to old)',
+        y_title='Compound score')
     line_chart.y_labels = [-1, -0.05, 0, 0.05, 1]
     line_chart.x_labels = [*range(1, len(tweet_scores) + 1)]
-
+    line_chart.add('Compound score', tweet_scores)
     return line_chart.render_data_uri()
